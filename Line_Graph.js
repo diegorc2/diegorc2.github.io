285b126be5ccb65c9d25c55c448f8661eb72a6d9
diegorc2 <55335464+diegorc2@users.noreply.github.com>
2025-08-10 09:57:13 -0500

    Update Line_Graph.js

--- Line_Graph.js
+++ Line_Graph.js
@@ -9,4 +9,4 @@ axisLeft = d3.scalelog().domain(0, d3.max(data.cases)).range(0,300);
 axisBottom = d3.scalelog().domain(0, d3.max(data.deaths)).range(0,300);
 d3.select('svg').attr('width', 300).attr('height', 300).append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisLeft));
 d3.select('svg').attr('width', 300).attr('height', 300).append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisBottom));
-}
+};
