87b37bfa9617b8c79d67e13d0ab04e2ffbda6239
diegorc2 <55335464+diegorc2@users.noreply.github.com>
2025-08-10 09:34:23 -0500

    Add data

---
+++ Line_Graph.js
@@ -0,0 +1,7 @@
+async function init() {
+data = await d3.csv('us-states.csv')
+axisLeft = d3.scalelog().domain(0, d3.max(data.cases)).range(0,300)
+axisBottom = d3.scalelog().domain(0, d3.max(data.deaths)).range(0,300)
+d3.select('svg').append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisLeft))
+d3.select('svg').append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisBottom))
+}
