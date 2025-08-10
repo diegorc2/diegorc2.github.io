064fbdeeb13178d8dcc6adf2423993323b0e0c5f
diegorc2 <55335464+diegorc2@users.noreply.github.com>
2025-08-10 09:59:14 -0500

    Update Line_Graph.js

--- Line_Graph.js
+++ Line_Graph.js
@@ -5,8 +5,8 @@ data = await d3.csv('us-states.csv', function(d) {
 		deaths: +d.deaths
 	};
 });
-axisLeft = d3.scalelog().domain(0, d3.max(data.cases)).range(0,300);
-axisBottom = d3.scalelog().domain(0, d3.max(data.deaths)).range(0,300);
+axisLeft = d3.scalelog().domain(0, d3.max(cases)).range(0,300);
+axisBottom = d3.scalelog().domain(0, d3.max(deaths)).range(0,300);
 d3.select('svg').attr('width', 300).attr('height', 300).append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisLeft));
 d3.select('svg').attr('width', 300).attr('height', 300).append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisBottom));
 };
