e2e20d5523f582410b659442d2a2d761a6b63eb0
diegorc2 <55335464+diegorc2@users.noreply.github.com>
2025-08-10 09:51:36 -0500

    Update Line_Graph.js

--- Line_Graph.js
+++ Line_Graph.js
@@ -1,7 +1,12 @@
 async function init() {
-data = await d3.csv('us-states.csv')
-axisLeft = d3.scalelog().domain(0, d3.max(data.cases)).range(0,300)
-axisBottom = d3.scalelog().domain(0, d3.max(data.deaths)).range(0,300)
-d3.select('svg').attr('width', 300).attr('height', 300).append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisLeft))
-d3.select('svg').attr('width', 300).attr('height', 300).append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisBottom))
+data = await d3.csv('us-states.csv', function(d) {
+	return {
+		cases: +d.cases,
+		deaths: +d.deaths
+	};
+};
+axisLeft = d3.scalelog().domain(0, d3.max(data.cases)).range(0,300);
+axisBottom = d3.scalelog().domain(0, d3.max(data.deaths)).range(0,300);
+d3.select('svg').attr('width', 300).attr('height', 300).append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisLeft));
+d3.select('svg').attr('width', 300).attr('height', 300).append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisBottom));
 }
