2e40bb1918131cbe2f944fba9cec5ca5e3e1ba60
diegorc2 <55335464+diegorc2@users.noreply.github.com>
2025-08-10 09:41:14 -0500

    Update Line_Graph.js

--- Line_Graph.js
+++ Line_Graph.js
@@ -2,6 +2,6 @@ async function init() {
 data = await d3.csv('us-states.csv')
 axisLeft = d3.scalelog().domain(0, d3.max(data.cases)).range(0,300)
 axisBottom = d3.scalelog().domain(0, d3.max(data.deaths)).range(0,300)
-d3.select('svg').append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisLeft))
-d3.select('svg').append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisBottom))
+d3.select('svg').attr('width', 300).attr('height', 300).append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisLeft))
+d3.select('svg').attr('width', 300).attr('height', 300).append('g').attr('transform', 'translate(50,50)').call(d3.axisLeft(axisBottom))
 }
